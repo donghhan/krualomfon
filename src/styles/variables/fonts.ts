e30f6b82bca3265d26d02e4fdd5e51fba4a3f3c6
donghhan <apensia914@gmail.com>
2023-12-05 20:50:52 +0700

    SCSS resetting & included icon image

---
+++ src/styles/variables/fonts.ts
@@ -0,0 +1,13 @@
+import { Charm, Kanit } from "next/font/google";
+
+export const charm = Charm({
+  subsets: ["thai"],
+  display: "swap",
+  weight: ["400", "700"],
+});
+
+export const kanit = Kanit({
+  subsets: ["thai"],
+  display: "swap",
+  weight: ["400", "600", "700"],
+});
